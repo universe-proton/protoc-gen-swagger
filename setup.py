5b4ef0f2dd7c3322f56e8095d0a44d496dd3e26a
余森彬 <yusenbin@laiye.com>
2018-01-29 17:59:25 +0800

    Add generated python code of swagger's annotation protobuf files. Adjust package version as 0.1.0 according to grpc-gateway version v1.3.1 .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '1.0.0'
+version = '0.1.0'
 
 
 class PyTest(TestCommand):
